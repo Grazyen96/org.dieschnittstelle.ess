4a5beed71fe43e6c4b610c00e4a2c87fcb7a138b
Khalil <med.khalil.hamila@gmail.com>
2026-04-15 13:56:42 +0200

    implement reflection-based attribute retrieval in ShowAnnotations #BAS2

--- org.dieschnittstelle.ess.bas/src/main/java/org/dieschnittstelle/ess/basics/ShowAnnotations.java
+++ org.dieschnittstelle.ess.bas/src/main/java/org/dieschnittstelle/ess/basics/ShowAnnotations.java
@@ -4,6 +4,12 @@ package org.dieschnittstelle.ess.basics;
 import org.dieschnittstelle.ess.basics.annotations.AnnotatedStockItemBuilder;
 import org.dieschnittstelle.ess.basics.annotations.StockItemProxyImpl;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.dieschnittstelle.ess.basics.reflection.ReflectedStockItemBuilder.getAccessorNameForField;
 import static org.dieschnittstelle.ess.utils.Utils.*;
 
 public class ShowAnnotations {
@@ -32,6 +38,39 @@ public class ShowAnnotations {
 		show("class is: " + instance.getClass());
 
 		try {
+
+			Map<String, String> instanceAttributes = new HashMap<String, String>();
+			Map<String, String> instanceAttributes2 = new HashMap<String, String>();
+
+			StringBuilder result= new StringBuilder("{" + instance.getClass().getSimpleName() + " ");
+
+			for (Field field : instance.getClass().getDeclaredFields()) {
+//				show("accessibility before: " + field.isAccessible());
+				field.setAccessible(true);
+//				show("accessibility after: " + field.canAccess(instance));
+
+				instanceAttributes.put(field.getName(), field.get(instance).toString());
+			}
+
+			for (Field field : instance.getClass().getDeclaredFields()) {
+				String getterName= getAccessorNameForField("get",field.getName());
+//				show("getName:" + getterName);
+				Method getter = instance.getClass().getDeclaredMethod(getterName);
+//				show (" getter: " + getter.invoke(instance));
+				result.append(field.getName()).append(":").append(getter.invoke(instance)).append(", ");
+				instanceAttributes2.put(field.getName(), getter.invoke(instance).toString());
+			}
+			result.deleteCharAt(result.length()-2);
+			result.append("}");
+
+
+
+			show("instance attributes dirty: {" + instance.getClass().getName() + " " + instanceAttributes.keySet() + "}");
+			show("instance attributes getter way: " + instance.getClass().getSimpleName() + " " + instanceAttributes2);
+//			this is the answer
+			show("instance attributes result string: " + result);
+
+
 
 			// TODO BAS2: create a string representation of instance by iterating
 			//  over the object's attributes / fields as provided by its class
