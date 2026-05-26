c7c44cb62d0fe81af249c6061f806273ef0832d4
Khalil <med.khalil.hamila@gmail.com>
2026-05-26 16:23:37 +0200

    refactor product CRUD operations to use AbstractProduct, all tests passed #JRS3

--- org.dieschnittstelle.ess.jrs.api/src/main/java/org/dieschnittstelle/ess/jrs/IProductCRUDService.java
+++ org.dieschnittstelle.ess.jrs.api/src/main/java/org/dieschnittstelle/ess/jrs/IProductCRUDService.java
@@ -1,6 +1,7 @@
 package org.dieschnittstelle.ess.jrs;
 
 import jakarta.ws.rs.*;
+import org.dieschnittstelle.ess.entities.erp.AbstractProduct;
 import org.dieschnittstelle.ess.entities.erp.IndividualisedProductItem;
 import jakarta.ws.rs.core.MediaType;
 import java.util.List;
@@ -25,21 +26,21 @@ import java.util.List;
 public interface IProductCRUDService {
 
 	@POST
-	public IndividualisedProductItem createProduct(IndividualisedProductItem prod);
+	public AbstractProduct createProduct(AbstractProduct prod);
 
 	@GET
-	public List<IndividualisedProductItem> readAllProducts();
+	public List<AbstractProduct> readAllProducts();
 
 	@PUT
 	@Path("/{productId}")
-	public IndividualisedProductItem updateProduct(@PathParam("productId") long id,
-												   IndividualisedProductItem update);
+	public AbstractProduct updateProduct(@PathParam("productId") long id,
+												   AbstractProduct update);
 	@DELETE
 	@Path("/{productId}")
 	boolean deleteProduct(@PathParam("productId") long id);
 
 	@GET
 	@Path("/{productId}")
-	public IndividualisedProductItem readProduct(@PathParam("productId") long id);
+	public AbstractProduct readProduct(@PathParam("productId") long id);
 			
 }
--- org.dieschnittstelle.ess.jrs/src/main/java/org/dieschnittstelle/ess/jrs/ProductCRUDServiceImpl.java
+++ org.dieschnittstelle.ess.jrs/src/main/java/org/dieschnittstelle/ess/jrs/ProductCRUDServiceImpl.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.ws.rs.core.Context;
 import org.apache.logging.log4j.Logger;
 import org.dieschnittstelle.ess.entities.GenericCRUDExecutor;
+import org.dieschnittstelle.ess.entities.erp.AbstractProduct;
 import org.dieschnittstelle.ess.entities.erp.IndividualisedProductItem;
 
 import java.util.List;
@@ -18,31 +19,31 @@ public class ProductCRUDServiceImpl implements IProductCRUDService {
 	protected static Logger logger = org.apache.logging.log4j.LogManager.getLogger(TouchpointCRUDServiceImpl.class);
 
 
-  private GenericCRUDExecutor<IndividualisedProductItem> productCRUD;
+  private GenericCRUDExecutor<AbstractProduct> productCRUD;
 
 	public ProductCRUDServiceImpl(@Context ServletContext servletContext, @Context HttpServletRequest request) {
 		logger.info("<constructor>: " + servletContext + "/" + request);
 		// read out the dataAccessor
-		this.productCRUD = (GenericCRUDExecutor<IndividualisedProductItem>) servletContext.getAttribute("productCRUD");
+		this.productCRUD = (GenericCRUDExecutor<AbstractProduct>) servletContext.getAttribute("productCRUD");
 
 		logger.debug("read out the productCRUD from the servlet context: " + this.productCRUD);
 	}
 
 	@Override
-	public IndividualisedProductItem createProduct(
-			IndividualisedProductItem prod) {
+	public AbstractProduct createProduct(
+			AbstractProduct prod) {
 		return this.productCRUD.createObject(prod);
 	}
 
 	@Override
-	public List<IndividualisedProductItem> readAllProducts() {
+	public List<AbstractProduct> readAllProducts() {
 
 		return (List) this.productCRUD.readAllObjects();
 	}
 
 	@Override
-	public IndividualisedProductItem updateProduct(long id,
-			IndividualisedProductItem update) {
+	public AbstractProduct updateProduct(long id,
+			AbstractProduct update) {
 		return this.productCRUD.updateObject(update);
 	}
 
@@ -52,9 +53,9 @@ public class ProductCRUDServiceImpl implements IProductCRUDService {
 	}
 
 	@Override
-	public IndividualisedProductItem readProduct(long id) {
+	public AbstractProduct readProduct(long id) {
 
-		IndividualisedProductItem product = this.productCRUD.readObject(id);
+		AbstractProduct product = this.productCRUD.readObject(id);
 		if (product == null) {
 			throw new jakarta.ws.rs.NotFoundException("Product with id " + id + " not found");
 		}
